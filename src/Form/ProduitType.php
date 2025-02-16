0396173dd4f217e69943ca33e991408b697ef7ef
OumaymaaMlik <Oumayma.mlik@esprit.tn>
2025-02-16 23:42:39 +0100

    Creatin de crud reponse reclamation / produit /categorie

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Produit;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prix')
+            ->add('description')
+            ->add('image')
+            ->add('quantite')
+            ->add('date_expiration', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'nomCategorie', // Affiche le nom de la catégorie
+                'placeholder' => 'Sélectionnez une catégorie',
+            ])
+            
+            ->add('utilisateur', EntityType::class, [
+                'class' => Utilisateur::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
